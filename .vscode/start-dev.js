7fcb7e4cc38e7f5bd0a3d816afb3b7d62291490f
neetoons <neet.toons@gmail.com>
2024-11-30 09:39:44 -0400

    add: express server

--- .vscode/start-dev.js
+++ .vscode/start-dev.js
@@ -1,7 +1,10 @@
 /* eslint-disable no-console */
 const { fork, spawn } = require('child_process');
 const path = require('path');
-
+const express = require("express")
+const Router = require("express")
+const router = Router(); 
+const fs = require("fs-extra")
 main();
 
 async function main() {
@@ -23,12 +26,35 @@ async function main() {
     process.exit(1);
   });
 }
-
 async function startServer() {
-  const server = fork(path.resolve(__dirname, './start-server.js'));
-  server.on('error', (err) => {
-    console.log('SERVER ERROR', err);
-    server.kill(1);
-    process.exit(1);
+  const app = express();
+  const dir = path.join(__dirname, '../dist')
+  console.log(dir)
+  router.get("/:filename",async(req, res) => {
+    const filename = req.params.filename;
+    const filePath = path.join(__dirname, `../${filename}`);
+    try {
+      if (!await fs.pathExists(filePath)) {
+        res.status(404).json({ error: 'File not found' });
+      }
+  
+      return res.download(filePath);
+    } catch (error) {
+      console.error('Download error', { error });
+      res.status(500).json({ error: 'Download failed' });
+    }
+  })
+  app.use("/", router)
+  app.listen(5500, () => {
+    console.log(`Server running on port 5500`);
   });
 }
+//async function startServer() {
+//  const server = fork(path.resolve(__dirname, './start-server.js'));
+//  server.on('error', (err) => {
+//    console.log('SERVER ERROR', err);
+//    server.kill(1);
+//    process.exit(1);
+//  });
+//}
+//
